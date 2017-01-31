1feadf465b99c8c069a714134e16eee45be9054c
Robyn To <rto@usc.edu>
2017-01-31 13:29:52 -0800

    Rotating triangle example...colored correctly for milestone

--- assign1_coreOpenGL_starterCode/hw1-starterCode/hw1.cpp
+++ assign1_coreOpenGL_starterCode/hw1-starterCode/hw1.cpp
@@ -7,7 +7,7 @@ C++ starter code
 Student username: rto
 */
 
-#include <iostream>
+#include <iostream>																																																																																	// Robyn To 1310467641
 #include <cstring>
 #include "openGLHeader.h"
 #include "glutHeader.h"
@@ -31,39 +31,86 @@ char shaderBasePath[1024] = "../openGLHelper-starterCode";
 #endif
 
 using namespace std;
+static GLfloat spin = 0.0;
+static GLfloat zStudent = 3 + (1310457641 / 10000000000);
+
+void spinDisplay(void)
+{
+	spin = spin + 2.0;
+	if (spin > 360.0)
+	{
+		spin = spin - 360.0;
+	}
+	glutPostRedisplay();
+}
+
+void reshape(int w, int h)
+{
+	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	
+	glOrtho(-50.0, 50.0, -50.0, 50.0, -1.0, 1.0);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
 
 void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_POLYGON);
-	glVertex3f(0.25, 0.25, 0.0);
-	glVertex3f(0.75, 0.25, 0.0);
-	glVertex3f(0.75, 0.75, 0.0);
-	glVertex3f(0.25, 0.75, 0.0);
-	glEnd();
+	glPushMatrix();
+	glRotatef(spin, 0.0, 0.0, 1.0);
+	glBegin(GL_TRIANGLES);
+		glColor3f(0.0, 0.0, 1.0);
+		glVertex3f(0.0, 0.0, 0.0);
 
-	glFlush();
+		glColor3f(1.0, 0.0, -1.0);
+		glVertex3f(0.0, 25, 0.0);
+
+		glColor3f(0.0, 1.0, 1.0);
+		glVertex3f(25, 0.0, 0.0);
+	glEnd();
+	glPopMatrix();
+	glutSwapBuffers();
+	//glFlush();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+	switch (button) {
+		case GLUT_LEFT_BUTTON:
+			if (state == GLUT_DOWN)
+				glutIdleFunc(spinDisplay);
+			break;
+		case GLUT_RIGHT_BUTTON:
+			if (state == GLUT_DOWN)
+				glutIdleFunc(NULL);
+			break;
+		default:
+			break;
+	}
 }
 
 void init(void)
 {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	glShadeModel(GLU_SMOOTH);
+//	glMatrixMode(GL_PROJECTION);
+	//glLoadIdentity();
+	//glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(250, 250);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("hello");
 	init();
 	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouse);
 	glutMainLoop();
 	return 0;
 }
